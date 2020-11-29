dbfef905b80f002b0726b9369e055183b42f0dc2
Syed Rakib Al Hasan <975768+syedrakib@users.noreply.github.com>
2020-11-29 18:56:52 +0800

    Remove all TF files

--- outputs.tf
+++
@@ -1,4 +0,0 @@
-output "sa_roles" {
-  description = "All roles (except sensitive roles filtered by the module) that are attached to the ServiceAccount generated for this Bastion Host."
-  value       = module.vm_instance.sa_roles
-}
